6addcc855a08b9519c764c64fa0d3bfefa75e2c8
Allen Madsen <blatyo@gmail.com>
2014-08-08 21:05:33 -0400

    Remove config option for golint

--- lib/permpress/golint.rb
+++ lib/permpress/golint.rb
@@ -4,7 +4,6 @@ require_relative 'command'
 module Permpress
   class GoLint < Thor
     desc 'lint [FILES]', 'Runs linter'
-    method_option :config, banner: 'CONFIG_FILE'
 
     def lint(*files)
       Command.new('fgt golint', files, []).run
